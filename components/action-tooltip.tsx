42bdb3b4fc2b94c2c81480365436744a212c6118
fffk10 <warafffk10@gmail.com>
2024-04-15 02:54:53 +0900

    navigation sidebar 実装中

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@radix-ui/react-tooltip'
+
+interface ActionTootipProps {
+  label: string
+  children: React.ReactNode
+  side?: 'top' | 'bottom' | 'left' | 'right'
+  align?: 'start' | 'center' | 'end'
+}
+
+const ActionTootip = ({ label, children, side, align }: ActionTootipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className='font-semibold text-sm capitalize'>
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+}
+
+export default ActionTootip
