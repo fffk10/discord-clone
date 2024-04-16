212dbd89e5f74994a7b1409162fc6030e2b4a5d4
fffk10 <warafffk10@gmail.com>
2024-04-17 03:31:32 +0900

    server settings(更新処理エラーあり)

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -49,7 +49,10 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className='text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer'>
+          <DropdownMenuItem
+            onClick={() => onOpen('editServer', { server })}
+            className='text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer'
+          >
             Server Settings
             <Settings className='h-4 2-4 ml-auto' />
           </DropdownMenuItem>
