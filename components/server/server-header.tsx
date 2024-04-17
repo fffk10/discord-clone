1336bd89bf47c682c5c7a02e7442c452d7acabda
fffk10 <warafffk10@gmail.com>
2024-04-18 02:40:56 +0900

    manage member modal実装途中

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -58,7 +58,10 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className='text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer'>
+          <DropdownMenuItem
+            onClick={() => onOpen('members', { server })}
+            className='text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer'
+          >
             Manage Members
             <Users className='h-4 2-4 ml-auto' />
           </DropdownMenuItem>
