68907ce5bc25d53aaf7002aca3bd762f73ac86dd
fffk10 <warafffk10@gmail.com>
2024-04-19 02:45:58 +0900

    manage memberモーダル実装途中

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -11,16 +11,41 @@ import { useModal } from '@/hooks/use-modal-store'
 import { ServerWithMembersWithProfiles } from '@/types'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { UserAvatar } from '@/components/user-avatar'
+import {
+  MoreVertical,
+  ShieldAlert,
+  ShieldCheck,
+  ShieldQuestion,
+} from 'lucide-react'
+import { useState } from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuTrigger,
+  DropdownMenuSubTrigger,
+} from '@/components/ui/dropdown-menu'
+
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className='h-4 w-4 ml-2 text-indigo-500' />,
+  ADMIN: <ShieldAlert className='h-4 w-4 text-rose-500' />,
+}
 
 export const MembersModal = () => {
   const { onOpen, isOpen, onClose, type, data } = useModal()
+  const [loadingId, setLoadingId] = useState('')
 
   const isModalOpen = isOpen && type === 'members'
   const { server } = data as { server: ServerWithMembersWithProfiles }
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-      <DialogContent className='bg-white text-black p-0 overflow-hidden'>
+      <DialogContent className='bg-white text-black overflow-hidden'>
         <DialogHeader className='pt-8 px-6'>
           <DialogTitle className='text-2xl font-bold'>
             Manage Members
@@ -33,6 +58,36 @@ export const MembersModal = () => {
           {server?.members?.map((member) => (
             <div key={member.id} className='flex items-center gap-x-2 mb-6'>
               <UserAvatar src={member.profile.imageUrl} />
+              <div className='flex flex-col gap-y-1'>
+                <div className='text-xs font-semibold flex items-center gap-x-1'>
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+                <p className='text-xs text-zinc-500'>{member.profile.email}</p>
+              </div>
+              {server.profileId !== member.profileId &&
+                loadingId !== member.id && (
+                  <div className='ml-auto'>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <MoreVertical className='h-4 w-4 text-zinc-500' />
+                        <DropdownMenuContent side='left'>
+                          <DropdownMenuSub>
+                            <DropdownMenuSubTrigger className='flex items-center'>
+                              <ShieldQuestion className='w-4 h-4 mr-2' />
+                              <span>Role</span>
+                            </DropdownMenuSubTrigger>
+                            <DropdownMenuPortal>
+                              <DropdownMenuSubContent>
+                                <DropdownMenuItem>Guest</DropdownMenuItem>
+                              </DropdownMenuSubContent>
+                            </DropdownMenuPortal>
+                          </DropdownMenuSub>
+                        </DropdownMenuContent>
+                      </DropdownMenuTrigger>
+                    </DropdownMenu>
+                  </div>
+                )}
             </div>
           ))}
         </ScrollArea>
