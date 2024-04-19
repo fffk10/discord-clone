f0a8ba7a7860c8530a49ca0138d1dc766039ba98
fffk10 <warafffk10@gmail.com>
2024-04-20 03:30:50 +0900

    不要ライブラリ削除

--- components/modals/leave-server-modal.tsx
+++ components/modals/leave-server-modal.tsx
@@ -9,13 +9,7 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog'
 import { useModal } from '@/hooks/use-modal-store'
-import { Label } from '@/components/ui/label'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { Check, Copy, RefreshCw } from 'lucide-react'
-import { useOrigin } from '@/hooks/use-origin'
 import { useState } from 'react'
-import axios from 'axios'
 
 export const LeaveServerModal = () => {
   const { onOpen, isOpen, onClose, type, data } = useModal()
