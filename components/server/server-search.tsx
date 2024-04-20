9b3954f24ff875848e167b023aafca28b560c185
fffk10 <warafffk10@gmail.com>
2024-04-20 15:47:59 +0900

    search server modal実装

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,11 +1,15 @@
+'use client'
+
 import {
   CommandDialog,
   CommandEmpty,
   CommandInput,
   CommandList,
 } from '@/components/ui/command'
+import { CommandGroup, CommandItem } from '@/components/ui/command'
 import { Search } from 'lucide-react'
-import { useState } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 interface ServerSearchProps {
   data: {
@@ -23,13 +27,44 @@ interface ServerSearchProps {
 
 export const ServerSearch = ({ data }: ServerSearchProps) => {
   const [open, setOpen] = useState(false)
+  const router = useRouter()
+  const params = useParams()
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        setOpen((open) => !open)
+      }
+    }
+    document.addEventListener('keydown', down)
+    return () => document.removeEventListener('keydown', down)
+  }, [])
+
+  const onClick = ({
+    id,
+    type,
+  }: {
+    id: string
+    type: 'channel' | 'member'
+  }) => {
+    setOpen(false)
+    if (type === 'member') {
+      return router.push(`/servers/${params?.serverId}/conversations/${id}`)
+    }
+
+    if (type === 'channel') {
+      return router.push(`/servers/${params?.serverId}/channels/${id}`)
+    }
+  }
+
   return (
     <>
       <button
         onClick={() => setOpen(true)}
         className='group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition'
       >
-        <Search className='w-4 h-4text-zinc-500 dark:text-zinc-400' />
+        <Search className='w-4 h-4 text-zinc-500 dark:text-zinc-400' />
         <p className='font-semibold text-sm text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition'>
           Search
         </p>
@@ -37,10 +72,24 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
           <span className='text-xs'>⌘</span>K
         </kbd>
       </button>
-      <CommandDialog>
+      <CommandDialog open={open} onOpenChange={setOpen}>
         <CommandInput placeholder='Search all channels and members' />
         <CommandList>
           <CommandEmpty>No Results found</CommandEmpty>
+          {data.map(({ label, type, data }) => {
+            if (!data?.length) return null
+
+            return (
+              <CommandGroup key={label} heading={label}>
+                {data?.map(({ id, icon, name }) => (
+                  <CommandItem key={id} onSelect={() => onClick({ id, type })}>
+                    {icon}
+                    <span>{name}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            )
+          })}
         </CommandList>
       </CommandDialog>
     </>
